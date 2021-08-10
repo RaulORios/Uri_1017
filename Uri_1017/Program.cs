0f465d33c83480b0bc4772c5c6a8dddd9553dce2
RaulORios <raul.orios89@outlook.com>
2021-08-10 11:47:19 -0300

    Arquivos atualizados

---
+++ Uri_1017/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri_1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int km_litro = 12;
+            int tempo = int.Parse(Console.ReadLine());
+            int velocidade = int.Parse(Console.ReadLine());
+
+            double tot_litros = (double)(tempo * velocidade) / km_litro;
+
+            Console.WriteLine(tot_litros.ToString("F3",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
